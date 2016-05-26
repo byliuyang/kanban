33be3654c144634296e813facb9fac86d106063b
Harry Liu <byliuyang11@gmail.com>
2016-05-26 15:03:07 +0800

    Refactor render to use state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import uuid from 'node-uuid';
 
 export default class App extends React.Component {
-    render() {
-        const notes = [
-            {
-                id: uuid.v4(),
-                task: 'Learn webpack'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Learn React'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Launch'
-            }
-        ];
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Launch'
+                }
+            ]
+        };
+    }
 
+    render() {
+        const notes = this.state.notes;
         return <div>
             <ul>{notes.map((note =>
                 <li key={note.id}>{note.task}</li>))}
