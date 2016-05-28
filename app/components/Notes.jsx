c3259ed8482e6f5f37689498178e34092975804b
Harry Liu <byliuyang11@gmail.com>
2016-05-28 10:12:24 +0800

    Attach classes to components

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -3,8 +3,9 @@ import Note from './Note'
 
 export default ({notes, onEdit, onDelete}) => {
     return (
-        <ul>{notes.map((note =>
-            <li key={note.id}>
+        <ul className="notes">{notes.map((note =>
+            <li className="note"
+                key={note.id}>
                 <Note task={note.task}
                       onEdit={onEdit.bind(null, note.id)}
                       onDelete={onDelete.bind(null, note.id)}></Note>
