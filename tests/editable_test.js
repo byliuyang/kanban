a1f9c2e7ab8a396a352c77d7098e62c3d0b15585
Harry Liu <byliuyang11@gmail.com>
2016-06-04 22:34:59 +0800

    Editable enters the edit mode

--- tests/editable_test.js
+++ tests/editable_test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {
     renderIntoDocument,
-    findRenderedDOMComponentWithClass
+    findRenderedDOMComponentWithClass,
+    Simulate
 } from 'react-addons-test-utils';
 import assert from 'assert';
 import Editable from '../app/components/Editable';
 
-describe('Editable', ()=> {
-    it('renders value', ()=> {
+describe('Editable', () => {
+    it('renders value', () => {
         const value = 'value';
         const component = renderIntoDocument(
             <Editable value={value}/>
@@ -16,5 +17,20 @@ describe('Editable', ()=> {
         const valueComponent = findRenderedDOMComponentWithClass(component, 'value');
 
         assert.equal(valueComponent.textContent, value);
+    });
+
+    it('triggers onValueClick', () => {
+        let triggered = false;
+
+        const value = 'value';
+        const onValueClick = () => triggered = true;
+        const component = renderIntoDocument(
+            <Editable value={value} onValueClick={onValueClick}/>
+        );
+
+        const valueComponent = findRenderedDOMComponentWithClass(component, 'value');
+        Simulate.click(valueComponent);
+
+        assert.equal(triggered, true);
     });
 });
