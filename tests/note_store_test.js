82e5de7666dc8766b182129999e28d866708c8c0
Harry Liu <byliuyang11@gmail.com>
2016-06-05 22:28:08 +0800

    test NoteStore allows update

--- tests/note_store_test.js
+++ tests/note_store_test.js
@@ -4,7 +4,11 @@ import NoteStore from '../app/stores/NoteStore';
 import alt from '../app/libs/alt';
 
 describe('NoteStore', () => {
-    it('create notes', () => {
+    beforeEach(() => {
+       alt.flush();
+    });
+
+    it('creates notes', () => {
        const task = 'test';
 
         NoteActions.create({task});
@@ -13,5 +17,21 @@ describe('NoteStore', () => {
 
         assert.equal(state.notes.length, 1);
         assert.equal(state.notes[0].task, task);
+    });
+
+    it('updates notes', () => {
+        const task = 'test';
+        const updatedTask = 'test 2';
+
+        NoteActions.create({task});
+
+        const note = NoteStore.getState().notes[0];
+
+        NoteActions.update({...note, task: updatedTask});
+
+        const state = NoteStore.getState();
+
+        assert.equal(state.notes.length, 1);
+        assert.equal(state.notes[0].task, updatedTask);
     });
 });
